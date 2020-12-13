4f514764914407909bf194a331ecfeecff07d7e3
Mike Repec <mikerepec@gmail.com>
2020-12-13 18:46:28 +0100

    Check auth in lambda and return dummy data

--- functions/products/index.js
+++ functions/products/index.js
@@ -1,7 +1,17 @@
 exports.handler = function (event, context) {
+  const { user } = context.clientContext
+  if (!user) {
+    return { statusCode: 401, body: 'Unauthorized' }
+  }
+
   // TODO: fetch a list of products
   return {
     statusCode: 200,
-    body: JSON.stringify([]),
+    headers: { 'content-type': 'application/json' },
+    body: JSON.stringify([
+      { id: '12', name: 'Nivea lotion', expiryDate: '2021-02-18' }, // TODO: replace dummy data
+      { id: '13', name: 'Shampoo', expiryDate: '2025-01-01' },
+      { id: '14', name: 'Shaving cream', expiryDate: '2021-06-01' },
+    ]),
   }
 }
