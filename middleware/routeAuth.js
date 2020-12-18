368f91e945f998c33fc5d9ca53d27f45f4ee630c
Mike Repec <mikerepec@gmail.com>
2020-12-18 23:48:36 +0100

    Add comment to routeAuth middleware

--- middleware/routeAuth.js
+++ middleware/routeAuth.js
@@ -1,6 +1,7 @@
 const publicRoutes = ['login']
 
 export default function ({ route, store, redirect }) {
+  // If user isn't authenticated and not accessing a public route, redirect to login page
   if (!publicRoutes.includes(route.name) && !store.state.auth.isLoggedIn) {
     return redirect('/login')
   }
