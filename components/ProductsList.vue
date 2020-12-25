a477bb213207b11fbc1e56b8b11d7f446fbf316e
Mike Repec <mikerepec@gmail.com>
2020-12-25 17:15:57 +0100

    Setup products list refetching

--- components/ProductsList.vue
+++ components/ProductsList.vue
@@ -1,10 +1,10 @@
 <template>
   <div>
-    <div v-if="busy" class="text-center">
+    <div v-if="busy && !products.length" class="text-center">
       <b-spinner />
     </div>
 
-    <b-row v-else>
+    <b-row>
       <b-col v-for="p in products" :key="p.id" cols="12" sm="6" lg="4" class="mb-4">
         <product-list-item v-bind="p" @remove="$emit('remove', { ...p })" @edit="$emit('edit', { ...p })" />
       </b-col>
