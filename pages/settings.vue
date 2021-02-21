80f82fc34036a816f74bc3c0c6ee45d250dcbf88
Mike Repec <mikerepec@gmail.com>
2021-02-21 17:53:24 +0100

    Some more POC notifications settings stuff

--- pages/settings.vue
+++ pages/settings.vue
@@ -3,7 +3,7 @@
     <h4>Account Settings</h4>
 
     <div class="wrapper mt-5">
-      <b-row>
+      <b-row class="setting-row">
         <b-col cols="12" lg="6" class="settings-header">
           <div>Notifications</div>
           <small class="text-muted">
@@ -15,6 +15,30 @@
           <b-form-checkbox v-model="notifications" name="notifications" switch>Enable notifications</b-form-checkbox>
         </b-col>
       </b-row>
+
+      <template v-if="notifications">
+        <b-row class="setting-row">
+          <b-col cols="12" lg="6" class="settings-header">
+            <div>Webhook</div>
+            <small class="text-muted">Get notified about an expiring product via custom webhook.</small>
+          </b-col>
+
+          <b-col>
+            <b-form-input v-model="webhook" size="sm" />
+          </b-col>
+        </b-row>
+
+        <b-row class="setting-row">
+          <b-col cols="12" lg="6" class="settings-header">
+            <div>Email</div>
+            <small class="text-muted">Receive an email whenever any of your products are about to expire.</small>
+          </b-col>
+
+          <b-col>
+            <b-form-input v-model="email" size="sm" type="email" />
+          </b-col>
+        </b-row>
+      </template>
     </div>
   </div>
 </template>
@@ -24,6 +48,9 @@ export default {
   data() {
     return {
       notifications: false,
+      webhook: '',
+      // TODO: get user's default email from their auth token
+      email: 'john.d@example.com',
     }
   },
 }
@@ -38,6 +65,11 @@ export default {
   display: block;
   line-height: 1rem;
   margin-top: 0.5rem;
+}
+
+.setting-row {
+  align-items: center;
+  margin-bottom: 1.5rem;
 }
 
 @media only screen and (min-width: 992px) {
